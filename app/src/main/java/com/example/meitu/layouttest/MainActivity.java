9aeafb84ed72fc5f1271f61d646437f025cc167d
罗浩阳 <lhy@meitu.com>
2017-05-04 09:51:21 +0800

    【优化】去除多余引用

--- app/src/main/java/com/example/meitu/layouttest/MainActivity.java
+++ app/src/main/java/com/example/meitu/layouttest/MainActivity.java
@@ -1,18 +1,7 @@
 package com.example.meitu.layouttest;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 
 public class MainActivity extends Activity {
 
